5d044bae09790d1f7714d7c479e77e03f62b966b
Rohit Tiwari <rotiwari238@gmail.com>
2025-08-03 15:27:59 +0530

    commit message

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ export const Navbar = ({menuOpen, setMenuOpen}) => {
                <div className="max-w-5xl mx-auto px-4">
                 <div className="flex justify-between items-center h-16">
                     <a href="#home" className="font-mono text-xl font-bold text-white">
-                        pedro<span className="text-blue-500">.tech</span>
+                        Rohit<span className="text-blue-500">.tech</span>
                     </a>
 
                     <div className="w-7 h-5 relative cursor-pointer z-40 md:hidden"
